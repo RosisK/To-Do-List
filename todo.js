fbe1a7ee35611839720651959ae36ed63f5a0c8b
RosisK <rosiskharel5@gmail.com>
2024-08-25 15:28:24 +0545

    Added a few css styles, but nothing major. Also modified js a bit.

--- todo.js
+++ todo.js
@@ -6,19 +6,30 @@ const taskList = document.querySelector(".task-list");
 const addTask = () => {
     const task = inputElement.value;
 
+    const listDiv = document.createElement("div");
+    listDiv.classList.add("task-list-div");
     const listItem = document.createElement("li");
+    listItem.classList.add("task-list-item");
     const removeTask = document.createElement("button");
+    removeTask.classList.add("remove-task-button");
 
     listItem.innerHTML = `${task}`;
     removeTask.innerHTML = "Remove Task";
 
-    taskList.appendChild(listItem);
-    taskList.appendChild(removeTask);
+    taskList.appendChild(listDiv);
+    listDiv.appendChild(listItem);
+    listDiv.appendChild(removeTask);
+
+    inputElement.value = "";
     
     removeTask.addEventListener("click", () => {
-        taskList.removeChild(listItem);
-        taskList.removeChild(removeTask);
+        taskList.removeChild(listDiv);
     });
 }
 
 addTaskButton.addEventListener("click", addTask);
+inputElement.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
